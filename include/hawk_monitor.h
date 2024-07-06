97e701899d169dce47a75ff3945f66cee1ea1789
Ankush <mondalankush9851@gmail.com>
2024-07-07 01:28:24 +0530

    monitor.h correctly alligned

--- include/hawk_monitor.h
+++ include/hawk_monitor.h
@@ -1,26 +1,23 @@
 #ifndef HAWK_MONITOR_H
 #define HAWK_MONITOR_H
 
-/*  inotify will be used for the monitoring of the /var/log/apt/history.log
- *
- */
+/* inotify will be used for the monitoring of the /var/log/apt/history.log */
 
-//#define FILEPATH /var/log/apt/history.log
-
-/* necessary headers included */
 #include <sys/inotify.h>
 #include <fcntl.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+
+
+
+
+
 /*  
  * The buffer given to read() should be at least
- * 
  * (sizeof(struct inotify_event) + NAME_MAX + 1) bytes, 
- *
  * where NAME_MAX is the maximum length of a filename, 
- *
  * plus one for the terminating null byte.
  *
 */
@@ -29,6 +26,9 @@
 
 
 char event_buffer[BUFFER_LEN] __attribute__((aligned(8)));              // buffer to hold the inotify event
+
+
+
 
 
 void hawk_monitoring_service_init(char* filename);
